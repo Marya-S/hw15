d3121a23754500911c25ea45f204457609042c21
Mary <7333581>
2022-08-01 22:16:02 +0400

    Добавлена библиотека StringUtils. Изменена проверка параметров

--- src/main/java/pro/sky/employee/EmployeeController.java
+++ src/main/java/pro/sky/employee/EmployeeController.java
@@ -1,5 +1,8 @@
 package pro.sky.employee;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -14,34 +17,31 @@ public class EmployeeController {
     }
 
     @GetMapping(path = "/add")
-    public Employee add(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname,
-                        @RequestParam(value = "salary") double salary,@RequestParam(value = "department") int department) {
-        if (!(name == null) && (!(surname == null))) {
-            return employeeService.addEmployee(name, surname, salary, department);
+    public ResponseEntity add(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname,
+                        @RequestParam(value = "salary") double salary, @RequestParam(value = "department") int department) {
+        if (!StringUtils.isAllEmpty(name, surname) && StringUtils.isAlpha(name) && StringUtils.isAlpha(surname)) {
+            return new ResponseEntity(employeeService.addEmployee(StringUtils.capitalize(name), StringUtils.capitalize(surname), salary, department), HttpStatus.OK);
         } else {
-            System.out.println("Не все параметры заданы");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad request");
         }
-        return null;
     }
 
-   @GetMapping(path = "/remove")
-    public Employee remove(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname) {
-        if (!(name == null) && (!(surname == null))) {
-            return employeeService.deleteEmployee(name, surname);
+    @GetMapping(path = "/remove")
+    public ResponseEntity remove(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname) {
+        if (!StringUtils.isAllEmpty(name, surname)) {
+            return new ResponseEntity(employeeService.deleteEmployee(name, surname), HttpStatus.OK);
         } else {
-            System.out.println("Не все параметры заданы");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad request");
         }
-        return null;
     }
 
     @GetMapping(path = "/find")
-    public Employee find(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname) {
-        if (!(name == null) && (!(surname == null))) {
-            return employeeService.findEmployee(name, surname);
+    public ResponseEntity find(@RequestParam(value = "firstName", required = false) String name, @RequestParam(value = "lastName", required = false) String surname) {
+        if (!StringUtils.isAllEmpty(name, surname)) {
+            return new ResponseEntity(employeeService.findEmployee(name, surname), HttpStatus.OK);
         } else {
-            System.out.println("Не все параметры заданы");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bad request");
         }
-        return null;
     }
 
     @GetMapping(path = "/getAll")
