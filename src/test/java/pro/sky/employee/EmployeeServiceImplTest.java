1422c7805dbb66083519468bec066a8c0f09475b
Mary <7333581>
2023-01-21 23:42:15 +0400

    Добавлены тесты JUnit и mockito

--- src/test/java/pro/sky/employee/EmployeeServiceImplTest.java
+++ src/test/java/pro/sky/employee/EmployeeServiceImplTest.java
@@ -1,9 +1,12 @@
 package pro.sky.employee;
 
 import exception.EmployeeAlreadyAdded;
+import exception.EmployeeNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,18 +28,38 @@ class EmployeeServiceImplTest {
     void addEmployeeIsExisted() {
         Employee person = employeeService.addEmployee("Сергей", "Петров", 18000, 1);
         assertTrue(person.getName().equals("Сергей") && person.getSurname().equals("Петров") && person.getSalary() == 15700 && person.getDepartment() == 1);
-        Assertions.assertThrows(EmployeeAlreadyAdded.class,() -> {employeeService.addEmployee("Сергей", "Петров", 18000, 1);;});
+        Assertions.assertThrows(EmployeeAlreadyAdded.class, () -> {
+            employeeService.addEmployee("Сергей", "Петров", 18000, 1);
+        });
     }
 
     @Test
     void deleteEmployee() {
+        employeeService.addEmployee("Виталий", "Семенов", 17000, 1);
+        employeeService.deleteEmployee("Виталий", "Семенов");
+        Assertions.assertThrows(EmployeeNotFoundException.class, () -> {
+            employeeService.findEmployee("Виталий", "Семенов");
+        });
+    }
+
+    @Test
+    void deleteIsNotExistedEmployee() {
+        Assertions.assertThrows(EmployeeNotFoundException.class, () -> {
+            employeeService.deleteEmployee("Абаракадабра", "Абраович");
+        });
     }
 
     @Test
     void findEmployee() {
+        employeeService.addEmployee("Лаврентий", "Филиппов", 17000, 1);
+        Employee person = employeeService.findEmployee("Лаврентий", "Филиппов");
+        Assertions.assertTrue(person.getName().equals("Лаврентий") && person.getSurname().equals("Филиппов")
+                && person.getDepartment() == 1);
     }
 
     @Test
     void getAllEmployeeList() {
+        Map<String, Employee> employeeList = employeeService.getAllEmployeeList();
+        Assertions.assertNotNull(employeeList);
     }
 }
